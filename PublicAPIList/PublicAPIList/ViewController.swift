8192f04a07371066a84cbf517992c7daa81c78a0
Bindu Ojha <prakashojha2005@gmail.com>
2022-04-14 10:15:11 +1000

    Initial Project SetUp

---
+++ PublicAPIList/PublicAPIList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PublicAPIList
+//
+//  Created by Saumya Prakash on 14/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
